5d9d5a188bcc426822e768407c098dfffab3e9a3
Jaradrj <jara.derooij@edu.tbz.ch>
2025-05-13 16:01:22 +0200

    add button and js

---
+++ Aufgabe 1 - Recipe/script.js
@@ -0,0 +1,5 @@
+function ExpandRecipe() {
+    var T = document.getElementById("container__optional");
+    T.style.display = "block";  
+    alert('Coming soon! But for now, just add some spices, half a lemon and a pinch of salt to your guacamole :)'); 
+}
