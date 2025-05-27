0b4e0fd8cebad8514d9156563098a8ec279e5f36
Jaradrj <jara.derooij@edu.tbz.ch>
2025-05-27 14:13:15 +0200

    update html and CSS too match objectives
    html-semantic structure and mockup

--- Aufgabe 2 - Collection/script.js
+++ Aufgabe 2 - Collection/script.js
@@ -1,17 +1,18 @@
 function redirectToSong(event) {
+    event.preventDefault();
     var songId = event.target.closest('.container__card').id;
     if (songId === 'borderline') {
-        window.location.href = "https://www.youtube.com/watch?v=2Vv-BfVoq4g";
+        window.location.href = "https://www.youtube.com/watch?v=2g5xkLqIElU";
     } else if (songId === 'creep') {
-        window.location.href = "https://www.youtube.com/watch?v=2Vv-BfVoq4g";
+        window.location.href = "https://www.youtube.com/watch?v=XFkzRNyygfk";
     } else if (songId === 'dietmountaindew') {
-        window.location.href = "https://www.youtube.com/watch?v=2Vv-BfVoq4g";
+        window.location.href = "https://www.youtube.com/watch?v=sEetXo3R-aM";
     } else if (songId === 'nothingelsematters') {
-        window.location.href = "https://www.youtube.com/watch?v=2Vv-BfVoq4g";
+        window.location.href = "https://www.youtube.com/watch?v=tAGnKpE4NCI";
     } else if (songId === 'perspectives') {
-        window.location.href = "https://www.youtube.com/watch?v=2Vv-BfVoq4g";
+        window.location.href = "https://www.youtube.com/watch?v=wtrixPKv5Gw";
     } else if (songId === 'wearethepeople') {
-        window.location.href = "https://www.youtube.com/watch?v=2Vv-BfVoq4g";
+        window.location.href = "https://www.youtube.com/watch?v=hN5X4kGhAtU";
     } else { 
         alert('Song not found');
     }
